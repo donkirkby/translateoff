e441333a413fee850327f081e52422717f82d5a3
Don Kirkby <donkirkby@gmail.com>
2018-04-07 22:10:26 -0700

    Tap buttons to build solution.

--- translateoff/app.py
+++ translateoff/app.py
@@ -24,9 +24,10 @@ class TranslateOffApp:
     def __init__(self):
         self._activity = android.PythonActivity.setListener(self)
         self.buttons = []
-        self.top_label = None
+        self.top_label = self.solution_label = None
         self.message = None
         self.sentence = None
+        self.shelf_layout = self.solution_layout = None
 
     def onCreate(self):
         r = Random()
@@ -43,16 +44,25 @@ class TranslateOffApp:
         self.top_label.setTextSize(50)
         vlayout.addView(self.top_label)
 
-        hlayout = LinearLayout(self._activity)
-        hlayout.setOrientation(LinearLayout.HORIZONTAL)
-        hlayout.setGravity(Gravity.CENTER)
+        self.solution_layout = LinearLayout(self._activity)
+        self.solution_layout.setOrientation(LinearLayout.HORIZONTAL)
+        self.solution_layout.setGravity(Gravity.LEFT)
+        self.solution_label = TextView(self._activity)
+        self.solution_label.setText('')
+        self.solution_label.setTextSize(50)
+        self.solution_layout.addView(self.solution_label)
+        vlayout.addView(self.solution_layout)
+
+        self.shelf_layout = LinearLayout(self._activity)
+        self.shelf_layout.setOrientation(LinearLayout.HORIZONTAL)
+        self.shelf_layout.setGravity(Gravity.CENTER)
         for text in self.sentence.get_buttons():
             button = Button(self._activity)
             button.setText(text)
             button.setTextSize(50)
-            hlayout.addView(button)
-        print('Buttons done.')
-        vlayout.addView(hlayout)
+            self.shelf_layout.addView(button)
+            button.setOnClickListener(ButtonClick(self.move_button, button))
+        vlayout.addView(self.shelf_layout)
 
         # self.button.setOnClickListener(ButtonClick(self.play))
         # self.button.setText('Click Me')
@@ -62,17 +72,26 @@ class TranslateOffApp:
         footer.setGravity(Gravity.CENTER)
         vlayout.addView(footer)
 
-        self.updateUI()
+        self.update_ui()
 
         self._activity.setContentView(vlayout)
 
-    def updateUI(self):
+    def update_ui(self):
         if self.message:
             self.top_label.setText(self.message)
 
-    def play(self):
-        self.message = 'Clicked 1!'
-        self.updateUI()
+    def move_button(self, button):
+        parent = button.getParent()
+        parent.removeView(button)
+        self.solution_layout.addView(button)
+        buttons = [self.solution_layout.getChildAt(i).getText()
+                   for i in range(1, self.solution_layout.getChildCount())]
+        if self.sentence.is_solved(buttons):
+            self.solution_label.setText(self.sentence.text)
+            while self.solution_layout.getChildCount() > 1:
+                self.solution_layout.removeView(
+                    self.solution_layout.getChildAt(1))
+        self.update_ui()
 
 
 def main():
