a015fcaf1c92469e4388a5f6b07c05c79994c0b4
Don Kirkby <donkirkby@gmail.com>
2018-04-09 21:48:19 -0700

    Add more sentences.

--- translateoff/app.py
+++ translateoff/app.py
@@ -5,11 +5,11 @@ import android
 # noinspection PyPackageRequirements
 from android.widget import LinearLayout, Button, TextView
 # noinspection PyPackageRequirements
-from android.view import Gravity, View
+from android.view import Gravity
 # noinspection PyPackageRequirements
 import android.view
 
-import translateoff.sentence
+from translateoff.sentence import Sentence
 
 
 # noinspection PyUnresolvedReferences,PyPep8Naming
@@ -24,9 +24,17 @@ class ButtonClick(implements=android.view.View[OnClickListener]):
 
 
 class Player:
-    def __init__(self, activity, is_inverted):
+    def __init__(self, activity, is_inverted, solved_callback):
+        """ Initialize.
+
+        :param activity: the main activity for the app
+        :param is_inverted: True if this player should be displayed upside
+            down
+        :param solved_callback: to call when the sentence is solved -
+            solved_callback(player)"""
         self.activity = activity
         self.is_inverted = is_inverted
+        self.solved_callback = solved_callback
         self.solution_buttons = []
         self.top_label = None
         self.solution_label = None
@@ -62,6 +70,12 @@ class Player:
     def set_sentence(self, sentence):
         self.sentence = sentence
         self.top_label.setText(self.sentence.translation)
+        self.solution_label.setText('')
+        self.solution_buttons.clear()
+        while self.solution_layout.getChildCount() > 1:
+            self.solution_layout.removeViewAt(1)
+        while self.shelf_layout.getChildCount():
+            self.shelf_layout.removeViewAt(0)
         for text in self.sentence.get_buttons():
             button = Button(self.activity)
             button.setText(text)
@@ -86,6 +100,7 @@ class Player:
             while self.solution_layout.getChildCount() > 1:
                 self.solution_layout.removeView(
                     self.solution_layout.getChildAt(1))
+            self.solved_callback(self)
 
 
 # noinspection PyPep8Naming
@@ -93,32 +108,66 @@ class TranslateOffApp:
     def __init__(self):
         self._activity = android.PythonActivity.setListener(self)
         self.players = []
+        self.sentences = []
+        self.next_button = None
 
     def onCreate(self):
         r = Random()
 
-        sentence = translateoff.sentence.Sentence('我想我可以。',
-                                                  'I think I can.',
-                                                  randrange=r.nextInt)
+        self.sentences = [Sentence('我想我可以。',
+                                   'I think I can.',
+                                   randrange=r.nextInt),
+                          Sentence('他在學中文。',
+                                   "He's studying Chinese.",
+                                   randrange=r.nextInt),
+                          Sentence('你不是學生。',
+                                   "You are not a student.",
+                                   randrange=r.nextInt)]
 
-        self.players = [Player(self._activity, is_inverted)
+        self.players = [Player(self._activity, is_inverted, self.on_solved)
                         for is_inverted in (True, False)]
         vlayout = LinearLayout(self._activity)
         vlayout.setOrientation(LinearLayout.VERTICAL)
 
         for i, player in enumerate(self.players):
             if i:
-                spacer = View(self._activity)
+                self.next_button = Button(self._activity)
+                self.next_button.setText('Start')
                 # noinspection PyUnresolvedReferences
-                spacer.setLayoutParams(LinearLayout.LayoutParams(
+                self.next_button.setLayoutParams(LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     1))
-                vlayout.addView(spacer)
+                self.next_button.setOnClickListener(
+                    ButtonClick(self.next_sentence))
+                vlayout.addView(self.next_button)
             player.on_create(vlayout)
-            player.set_sentence(sentence)
 
+        self.next_sentence()
         self._activity.setContentView(vlayout)
+
+    def on_solved(self, player):
+        print('on_solved', self.next_button.getText())
+        if self.next_button.getText():
+            return
+        if player.is_inverted:
+            self.next_button.setRotation(180)
+        else:
+            self.next_button.setRotation(0)
+        self.next_button.setText('Next')
+
+    def next_sentence(self):
+        print('next_sentence', self.next_button.getText(), len(self.sentences))
+        if not self.next_button.getText():
+            # Current sentence isn't solved.
+            return
+        self.next_button.setText('')
+        if not self.sentences:
+            # No more sentences.
+            return
+        sentence = self.sentences.pop()
+        for player in self.players:
+            player.set_sentence(sentence)
 
 
 def main():
